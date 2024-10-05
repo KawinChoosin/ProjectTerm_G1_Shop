b3de00dd3cbe39499b8ec1ca16dac59f9b4fa08e
KawinChoosin <110968575+KawinChoosin@users.noreply.github.com>
2024-10-06 00:49:33 +0700

    validate success

--- TProject/src/page/User/User.tsx
+++ TProject/src/page/User/User.tsx
@@ -46,6 +46,9 @@ function User() {
       T_pnum: '',
       C_gender: '',
       C_age: '',
+      O_pass: '',
+      N_pass: '',
+      RN_pass: '',
     },
   });
 
@@ -216,7 +219,7 @@ function User() {
               validate: (value) => !/\s/.test(value) || 'Name should not contain spaces',
             }}
             render={({ field }) => (
-              <>
+              
                 <TextField
                 fullWidth
                   sx={{ mb: 2,mt:1 }}
@@ -225,8 +228,9 @@ function User() {
                   {...field}
                   error={Boolean(errors.C_name)}
                   helperText={errors.C_name?.message}
+                  onBlur={() => trigger('C_name')} 
                 />
-              </>
+         
             )}
           />
           {/* <Controller
@@ -259,12 +263,12 @@ function User() {
             rules={{
               required: 'Phone number is required',
               pattern: {
-                value: /^[0-9]*$/,
-                message: 'Phone number must be numeric',
+                value: /^0[0-9]{9}$/, // Ensure number starts with 0 and has 10 digits
+                message: 'Phone number must start with 0 and be 10 digits long',
               },
             }}
             render={({ field }) => (
-              <>
+           
                 <TextField
                 fullWidth
                   sx={{ mb: 2 }}
@@ -273,14 +277,17 @@ function User() {
                   {...field}
                   error={Boolean(errors.T_pnum)}
                   helperText={errors.T_pnum?.message}
+                  onBlur={() => trigger('T_pnum')} 
                 />
-              </>
+             
             )}
           />
           <Controller
             name="C_gender"
             control={control}
-            rules={{ required: 'Gender is required' }}
+            rules={{
+               required: 'Gender is required',
+               }}
             render={({ field }) => (
               <>
                 <Select
@@ -307,6 +314,8 @@ function User() {
                 value: /^[0-9]*$/,
                 message: 'Age must be numeric',
               },
+              validate: (value) => value < 150 || 'Are you sure, Make sure the age is valid',
+
             }}
             render={({ field }) => (
               <>
@@ -318,6 +327,7 @@ function User() {
                   {...field}
                   error={Boolean(errors.C_age)}
                   helperText={errors.C_age?.message}
+                  onBlur={() => trigger('C_age')}
                 />
               </>
             )}
@@ -330,69 +340,114 @@ function User() {
       </Dialog>
 
       {/* Change Password Dialog */}
-      <Dialog open={changePass} onClose={() => setChangePass(false)}>
-        <DialogTitle>Change Password</DialogTitle>
-        <DialogContent>
-          <TextField
+      {/* Change Password Dialog */}
+<Dialog open={changePass} onClose={() => setChangePass(false)}>
+  <DialogTitle>Change Password</DialogTitle>
+  <DialogContent>
+    {/* Old Password */}
+    <Controller
+      name="O_pass"
+      control={control}
+      rules={{
+        required: 'Old password is required',
+      }}
+      render={({ field }) => (
+        <TextField
           fullWidth
-            sx={{ mb: 2,mt:1 }}
-            label="Old Password"
-            type={showOldPassword ? 'text' : 'password'}
-            variant="outlined"
-            value={password.O_pass}
-            onChange={(e) => setPassword({ ...password, O_pass: e.target.value })}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton onClick={() => setShowOldPassword((prev) => !prev)}>
-                    {showOldPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-          />
-          <TextField
-            fullWidth
-            sx={{ mb: 2,mt:1 }}
-            label="New Password"
-            type={showNewPassword ? 'text' : 'password'}
-            variant="outlined"
-            value={password.N_pass}
-            onChange={(e) => setPassword({ ...password, N_pass: e.target.value })}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton onClick={() => setShowNewPassword((prev) => !prev)}>
-                    {showNewPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-          />
-          <TextField
+          sx={{ mb: 2, mt: 1 }}
+          label="Old Password"
+          type={showOldPassword ? 'text' : 'password'}
+          variant="outlined"
+          {...field}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={() => setShowOldPassword((prev) => !prev)}>
+                  {showOldPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+          error={Boolean(errors.O_pass)}
+          helperText={errors.O_pass?.message}
+          onBlur={() => trigger('O_pass')} // Trigger validation on blur
+        />
+      )}
+    />
+
+    {/* New Password */}
+    <Controller
+      name="N_pass"
+      control={control}
+      rules={{
+        required: 'New password is required',
+        minLength: { value: 6, message: 'Password must be at least 6 characters long' },
+      }}
+      render={({ field }) => (
+        <TextField
           fullWidth
-            sx={{ mt:1 }}
-            label="Retype New Password"
-            type={showRetypePassword ? 'text' : 'password'}
-            variant="outlined"
-            value={password.RN_pass}
-            onChange={(e) => setPassword({ ...password, RN_pass: e.target.value })}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton onClick={() => setShowRetypePassword((prev) => !prev)}>
-                    {showRetypePassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button sx={{ mb: 2 }} onClick={() => setChangePass(false)}>Cancel</Button>
-          <Button sx={{ mb: 2}} onClick={handleChangePass}>Change Password</Button>
-        </DialogActions>
-      </Dialog>
+          sx={{ mb: 2, mt: 1 }}
+          label="New Password"
+          type={showNewPassword ? 'text' : 'password'}
+          variant="outlined"
+          {...field}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={() => setShowNewPassword((prev) => !prev)}>
+                  {showNewPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+          error={Boolean(errors.N_pass)}
+          helperText={errors.N_pass?.message}
+          onBlur={() => trigger('N_pass')} // Trigger validation on blur
+        />
+      )}
+    />
+
+    {/* Retype New Password */}
+    <Controller
+      name="RN_pass"
+      control={control}
+      rules={{
+        required: 'Please retype the new password',
+        validate: (value) =>
+          value === watch('N_pass') || 'Passwords do not match',
+      }}
+      render={({ field }) => (
+        <TextField
+          fullWidth
+          sx={{ mt: 1 }}
+          label="Retype New Password"
+          type={showRetypePassword ? 'text' : 'password'}
+          variant="outlined"
+          {...field}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={() => setShowRetypePassword((prev) => !prev)}>
+                  {showRetypePassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+          error={Boolean(errors.RN_pass)}
+          helperText={errors.RN_pass?.message}
+          onBlur={() => trigger('RN_pass')} // Trigger validation on blur
+        />
+      )}
+    />
+  </DialogContent>
+
+  <DialogActions>
+    <Button sx={{ mb: 2 }} onClick={() => setChangePass(false)}>Cancel</Button>
+    <Button sx={{ mb: 2 }} onClick={handleSubmit(handleChangePass)}>Change Password</Button>
+  </DialogActions>
+</Dialog>
+
+
     </Box>
     </Grid>
   );
